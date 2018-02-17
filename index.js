c98573ea477584d2630f8023a7c6d9d3c9ad469f
Jerry Brown <brownjer3@gmail.com>
2018-02-17 22:15:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function happyHolidays() {
-  return "Happy holidays!";
+  return "Happy holidays!"
 }
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, " + ${name} + "!";
+  return "Happy holidays, " + ${name} + "!"
 }
 
 function happyHolidayTo(holday, name) {
-  return "Happy, " + ${holiday} + ", " + ${name} + "!";
+  return "Happy, " + ${holiday} + ", " + ${name} + "!"
 }
 
 function holidayCountDown(days, holiday) {
-  return "It's " + ${days} + "days until " + ${holiday} + "!";
+  return "It's " + ${days} + "days until " + ${holiday} + "!"
 }
