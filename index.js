14b8e2a2391f3f38c5c3f88c79637e4e3501bd4e
Jerry Brown <brownjer3@gmail.com>
2018-02-17 22:09:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-
-
 function happyHolidaysTo(name) {
   return "Happy holidays, " + ${name} "!"
 }
+
+
+/*
 
 function happyHolidayTo(holday, name) {
   return "Happy, " ${holiday} ", " ${name}"!"
