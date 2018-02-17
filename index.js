b7d33f4bc59bc1b78cc8f33cdb148aa9f2efc9e8
Jerry Brown <brownjer3@gmail.com>
2018-02-17 22:13:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyHolidayTo(holday, name) {
 }
 
 function holidayCountDown(days, holiday) {
-  return "It's " ${days} "days until "${holiday} "!"
+  return "It's " + ${days} + "days until " + ${holiday} + "!"
 }
